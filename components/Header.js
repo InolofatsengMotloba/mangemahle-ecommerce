bfd63872973c23f7f0726d9fa8838dccf81e817b
InolofatsengMotloba <motlobainolo123@gmail.com>
2025-04-02 19:13:11 +0200

    Set Up Navigation

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,40 @@
+"use client";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { auth } from "../lib/firebase";
+import Logout from "./Logout";
+
 export default function Header() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <>
-      <h1>Header goes here</h1>
-    </>
+    <nav className="p-4 bg-gray-800 text-white flex justify-between">
+      <Link href="/">Home</Link>
+      <div>
+        {user ? (
+          <>
+            <Link href="/profile" className="mr-4">
+              Profile
+            </Link>
+            <Logout />
+          </>
+        ) : (
+          <>
+            <Link href="/signup" className="mr-4">
+              Sign Up
+            </Link>
+            <Link href="/signin">Sign In</Link>
+          </>
+        )}
+      </div>
+    </nav>
   );
 }
