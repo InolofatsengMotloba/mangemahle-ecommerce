b41d9a80315b0c7a6ea304e0a033eb17ba326344
InolofatsengMotloba <motlobainolo123@gmail.com>
2025-04-21 20:08:38 +0200

    Refactor SearchInput component: update layout and styles

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -4,20 +4,20 @@ import { FaSearch } from "react-icons/fa";
 
 const SearchInput = ({ query, setQuery }) => {
   return (
-    <div className="flex">
-      <div className="relative">
+    <div className="flex justify-center">
+      <div className="relative w-full max-w-md">
         <input
           type="text"
-          placeholder="Search products..."
+          placeholder="Search Products..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          className="flex-grow px-4 py-2 pl-6 text-gray-500 bg-white rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-[#2d7942] transition-all duration-300"
+          className="w-full pr-10 pl-5 py-2.5 text-sm text-gray-600 bg-white border border-gray-200 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-[#94bb9f] transition-all duration-300"
         />
         <button
           type="submit"
-          className="absolute right-0 p-3 bg-[#2d7942] rounded-full text-white  shadow-lg hover:bg-[#1d5931] transition-colors duration-300"
+          className="absolute top-1/2 -translate-y-1/2 right-2 p-2 bg-[#94bb9f] hover:bg-[#385941] text-white rounded-full transition-colors duration-300"
         >
-          <FaSearch />
+          <FaSearch size={14} />
         </button>
       </div>
     </div>
