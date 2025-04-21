9654da2d3872043c66226e0482f9bf23ef658892
InolofatsengMotloba <motlobainolo123@gmail.com>
2025-04-22 00:06:32 +0200

    Add Suspense wrapper around children in RootLayout component

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ import LocationPermission from "@/components/LocationPermissions";
 import { CartProvider } from "@/contexts/CartContext";
 import { Poppins } from "next/font/google";
 import BottomNav from "@/components/BottomNav";
+import { Suspense } from "react";
 
 // Use Poppins as the primary font
 const poppins = Poppins({
@@ -22,17 +23,20 @@ export default function RootLayout({ children }) {
       <body
         className={`${poppins.variable} font-sans antialiased flex flex-col min-h-screen`}
       >
-        <LocationProvider>
-          <CartProvider>
-            <div className="flex flex-col flex-1">
-              <Header />
-              <main className="flex-1">{children}</main>
-              {/* {!isHomePage && <Footer />} */}
-            </div>
-            <LocationPermission />
-            <BottomNav />
-          </CartProvider>
-        </LocationProvider>
+        {/* wrap children with Suspense */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <LocationProvider>
+            <CartProvider>
+              <div className="flex flex-col flex-1">
+                <Header />
+                <main className="flex-1">{children}</main>
+                {/* {!isHomePage && <Footer />} */}
+              </div>
+              <LocationPermission />
+              <BottomNav />
+            </CartProvider>
+          </LocationProvider>
+        </Suspense>
       </body>
     </html>
   );
